6ef754d6804ddaa22389edcb655a58c293bd3f0d
shoichimasuhara <shoichimasuhara@hatena.ne.jp>
2013-01-31 14:16:14 +0900

    remove garbages

--- src/lib/Parascript/Config.pm
+++
@@ -1,8 +0,0 @@
-package Parascript::Config;
-use strict;
-use warnings;
-use constant {
-    DEFAULT_RC  => $ENV{"HOME"} . '/.parascriptrc'
-};
-
-1;
--- src/lib/Parascript/Results.pm
+++
@@ -1,9 +0,0 @@
-package Parascript::Results;
-use strict;
-use warnings;
-use Class::Accessor::Lite;
-Class::Accessor::Lite->mk_accessor(qw(
-    ok_hosts ng_hosts
-));
-
-1;
